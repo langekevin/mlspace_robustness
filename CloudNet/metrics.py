aae66e50c583e0980d81e45776e16e8b601e6d92
langekevin <kevinlange97@icloud.com>
2024-01-08 20:52:08 +0100

    Create metrics.py

---
+++ CloudNet/metrics.py
@@ -0,0 +1,25 @@
+from tensorflow.python.keras import backend as K
+
+
+epsilon = 0.0000001
+
+
+def jacc_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return 1 - ((intersection + epsilon) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + epsilon))
+
+
+def precision(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+
+def recall(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
