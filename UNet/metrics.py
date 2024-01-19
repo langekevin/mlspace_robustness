c97351d45b2504774517895153279cff3e373420
langekevin <kevinlange97@icloud.com>
2024-01-19 21:44:11 +0100

    Update metrics.py

--- UNet/metrics.py
+++ UNet/metrics.py
@@ -5,6 +5,18 @@ epsilon = 0.0000001
 
 
 def jacc_coef(y_true, y_pred):
+    """Calculates the Jaccard coefficient which is the intersection of the
+    ground truth and the predicted value divided by the sum of the predicted
+    and ground truth positions that are no part of the intersection
+
+    Args:
+        y_true (np.ndarray): The ground truth value (e. g. the image mask)
+        y_pred (np.ndarray): The predicted value (e. g. the image mask)
+
+    Returns:
+        float: A number between 0 and 1 where 0 indicates a perect match
+            between `y_true` and `y_pred` and 1 being completely different.
+    """
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
@@ -12,6 +24,18 @@ def jacc_coef(y_true, y_pred):
 
 
 def precision(y_true, y_pred):
+    """Calculates the precision value which describes the sum of all
+    true positives devided by the sum of predicted positives.
+
+    Args:
+        y_true (np.ndarray): The ground truth value (e. g. the image mask)
+        y_pred (np.ndarray): The predicted value (e. g. the image mask)
+
+    Returns:
+        float: A number between 0 and 1 where 1 indicates a perfect match
+            between the true positives and the predicted positives and 0
+            having no precision (e. g. true positives) at all.
+    """
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
     precision = true_positives / (predicted_positives + K.epsilon())
@@ -19,6 +43,18 @@ def precision(y_true, y_pred):
 
 
 def recall(y_true, y_pred):
+    """Calculates the recall value which describes the sum of all
+    true positives divided by the sum of all possible positive values
+
+    Args:
+        y_true (np.ndarray): The ground truth value (e. g. the image mask)
+        y_pred (np.ndarray): The predicted value (e. g. the image mask)
+
+    Returns:
+        float: A number between 0 and 1 where 1 indicates a perfect match
+            between the true positives and the possible positive values and
+            0 having not covered the possible positives at all.
+    """
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
     recall = true_positives / (possible_positives + K.epsilon())
