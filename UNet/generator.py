14f6a1f527942e478aacc3497f2f0cbe88ba7a30
langekevin <kevinlange97@icloud.com>
2024-01-19 20:42:00 +0100

    Update generator.py

--- UNet/generator.py
+++ UNet/generator.py
@@ -1,21 +1,38 @@
 import os
 import random
+from typing import List, Tuple, Generator
 import numpy as np
 from tqdm import tqdm
-from typing import List, Tuple
 from skimage.io import imread
 from skimage.transform import resize
 
 
-def get_batch(dataset_path: str, file_list: List[str], size: Tuple[int, int], batch_size: int) -> Tuple[np.ndarray, np.ndarray]:
+def get_batch(dataset_path: str, file_list: List[str], size: Tuple[int, int], batch_size: int) -> Generator[Tuple[np.ndarray, np.ndarray], None, None]:
+    """The function first loads all data from the disk into the memory to then create
+    batches of a specified batch_size during the training and validation process.
+
+    Args:
+        dataset_path (str): The path where the dataset is located
+        file_list (List[str]): The list of filenames that need to be loaded
+        size (Tuple[int, int]): The target dimensions of the images (resolution)
+        batch_size (int): The batch size that was used for the training process
+
+    Yields:
+        Generator[Iterator[Tuple[np.ndarray, np.ndarray]]]: Yields an iterator that creates
+            tuples where each tuple consists of the input images on the first index
+            and the ground truth on the second image
+    """
     n_batches = np.ceil(len(file_list) / batch_size)
     counter = 0
     data = []
-    
+
     for file in tqdm(file_list, desc="Loading Images", unit="file"):
         image = imread(os.path.join(dataset_path, f'Combined/ImagesSmall/{file}.tif'))
         mask = imread(os.path.join(dataset_path, f"Combined/MasksSmall/{file}.tif"))
-        
+
+        image = resize(image, size, preserve_range=True, mode='symmetric')
+        mask = resize(mask, size, preserve_range=True, mode='symmetric')
+
         image = (image / 255).astype(np.float32)
         mask = (mask / 255).astype(np.float32)
         mask = np.where(mask < 0.5, np.float32(0), np.float32(1))
